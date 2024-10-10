d59096c8372ca01379a37b6bc16e8a4ceb46fc4e
Mike <mike.kangaroo.world@gmail.com>
2024-10-10 23:58:02 +0900

    feat: Create vue-app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <input name="firstInput" type="number" v-model="firstInput" />
+    <input name="secondInput" type="number" v-model="secondInput" />
+    <input name="thirdInput" type="number" v-model="thirdInput" />
+    <div>Sum: {{ sum }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const firstInput = ref(0);
+const secondInput = ref(0);
+const thirdInput = ref(0);
+
+const sum = computed(
+  () => firstInput.value + secondInput.value + thirdInput.value
+);
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
